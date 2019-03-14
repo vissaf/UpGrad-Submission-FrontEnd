31e45c4bb407f0471c495909d899ad1441269931
Vishnu <safayavishnu@gmail.com>
2019-03-14 15:51:40 +0530

    Updates

---
+++ precache-manifest.6d391e546cbe303183a794dda2836cf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e0e0133e3aacffebc56",
+    "url": "/UpGrad-Submission-FrontEnd/static/js/runtime~main.03bfcc85.js"
+  },
+  {
+    "revision": "8e04b947d3be8966c781",
+    "url": "/UpGrad-Submission-FrontEnd/static/js/main.2636c2b1.chunk.js"
+  },
+  {
+    "revision": "bfb032c1efdfe9a9704f",
+    "url": "/UpGrad-Submission-FrontEnd/static/js/2.f5401510.chunk.js"
+  },
+  {
+    "revision": "8e04b947d3be8966c781",
+    "url": "/UpGrad-Submission-FrontEnd/static/css/main.ff715b11.chunk.css"
+  },
+  {
+    "revision": "2b0502804cd7c6f6173dcae9f4bcceac",
+    "url": "/UpGrad-Submission-FrontEnd/index.html"
+  }
+];
